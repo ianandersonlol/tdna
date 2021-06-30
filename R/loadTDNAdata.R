25f4a72bc951396900ac42a3b076325855b023de
Grey Monroe <greymonroe@Monroe-LM.local>
2021-06-30 06:00:27 -0700

    fix load

--- R/loadTDNAdata.R
+++ R/loadTDNAdata.R
@@ -10,7 +10,7 @@ loadTDNAdata<-function()
   gff<<-fread(system.file("extdata", "Araport11_GFF3_genes_transposons.201606.gff.gz", package = "tdna"))
   colnames(gff)<-c("chr","source","feature","start","stop","blank","strand","blank2","info")
   confirmed<<-fread(system.file("extdata", "sum_SALK_confirmed.txt.gz", package = "tdna"))
-  confirmed__exon_hom_sent<<-confirmed[Hit.region=="Exon" & HM %in% c("HMc","HMn") & ABRC != "NotSent"]
+  confirmed__exon_hom_sent<<-confirmed[`Hit region`=="Exon" & HM %in% c("HMc","HMn") & ABRC != "NotSent"]
   confirmed__exon_hom_sent$Target.Gene<<-toupper(confirmed__exon_hom_sent$Target.Gene)
   location<<-fread(system.file("extdata", "T-DNAall.Genes.Araport11.txt.gz", package = "tdna"))
   location$pos<<-as.numeric(sapply(location$V5, function(x) unlist(strsplit(unlist(strsplit(as.character(x), " vs "))[1], "-"))[1]))
